f05b5c4bc8fd4268e1fc74be1218e3ff1f025ff1
heski0709 <heski0709@gmail.com>
2023-05-19 16:05:21 +0900

    Fix: WebSecurityConfigurerAdapter 지원중단으로 인한 코드 수정 WebSecurityConfigurerAdpater 지원중단으로 상속 없앰 기존에 오버라이딩한 configure메소드를 securityFilterChain 메소드로 수정

--- 6.4-Frontend_Deployment_With_AWS_ElasticBeantalk/demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ 6.4-Frontend_Deployment_With_AWS_ElasticBeantalk/demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,37 +1,39 @@
 package com.example.demo.config;
 
-import com.example.demo.security.JwtAuthenticationFilter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 
+import com.example.demo.security.JwtAuthenticationFilter;
+
+import lombok.extern.slf4j.Slf4j;
+
 @EnableWebSecurity
-@Slf4j
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		// http 시큐리티 빌더
-		http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
-				.and()
-				.csrf()// csrf는 현재 사용하지 않으므로 disable
-						.disable()
-				.httpBasic()// token을 사용하므로 basic 인증 disable
-						.disable()
-				.sessionManagement()  // session 기반이 아님을 선언
-						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-				.authorizeRequests() // /와 /auth/** 경로는 인증 안해도 됨.
-						.antMatchers("/", "/auth/**").permitAll()
-				.anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
-						.authenticated();
+		http.cors(cors -> cors.and()) // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
+			.csrf(csrf -> 
+					csrf.disable())// csrf는 현재 사용하지 않으므로 disable
+			.httpBasic(basic -> 
+					basic.disable())// token을 사용하므로 basic 인증 disable
+			.sessionManagement(session -> 
+					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // session 기반이 아님을 선언
+			.authorizeHttpRequests(authz -> // /와 /auth/** 경로는 인증 안해도 됨.
+					authz.antMatchers("/", "/auth/**")
+					.permitAll()
+					.anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+					.authenticated());
 
 		// filter 등록.
 		// 매 리퀘스트마다
@@ -41,5 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 						jwtAuthenticationFilter,
 						CorsFilter.class
 		);
+
+		return http.build();
 	}
 }
